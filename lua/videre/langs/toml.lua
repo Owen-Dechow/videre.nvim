a9675506f5f2f469335f4846456ff52e0964f20d
Owen Dechow <owen.dechow@gmail.com>
2025-08-22 11:01:47 -0400

    fix [ ]# error

--- lua/videre/langs/toml.lua
+++ lua/videre/langs/toml.lua
@@ -7,11 +7,14 @@ end
 local M = {
     encode = nil,
     decode = function(toml_text)
+        toml_text = toml_text:gsub("^[ \t]+", ""):gsub("\n[ \t]+", "\n")
         local success, result = pcall(toml.parse, toml_text)
+
         if not success then
             error("Failed to parse TOML: " .. result)
         end
 
+        vim.print({ text=toml_text, success = success, result = result, tbl = "toml" })
         return result
     end,
     highlight = function()
